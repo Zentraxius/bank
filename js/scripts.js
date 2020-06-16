98ad1f3b8039d1afed0f73c30a1a9737fbed1ecc
Cody Fritz <clanalia55@gmail.com>
2020-06-16 15:57:54 -0700

    panic commit
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>
     co-authored by: Tristan Emmerson <tristan@stickerslug.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
-function Bank() {
- this.accountName = accountName;
- this.startingAmt = startingAmt;
- this.deposit = deposit;
- this.withdrawl = withdrawl;
-}
-
-Bank.prototype.startingAmt = function() {
-  
-}
-
-Bank.prototype.deposit = function() {
-  
-}
-
-Bank.prototype.withdrawl = function() {
-  
-}
 
 
-
-$(document).ready(function(){
-  $('form#new-account').submit(function(event) {
-    event.preventDefault();
-
-    let clientName = $('input#userName').val()
-    let initalBalance = ($('input#initialDeposit').val() + $('#giveMeMoney').val() - $('#takeMyMoney').val())
-
-
-    $('.balanceDisplay').text('$'+initalBalance)
-    $('.userName').text(clientName)
-  })
-})
+function Bank(name, deposit) {
+  this.accountName = name;
+  this.totalMoney = deposit;
+ }
+ 
+ Bank.prototype.startingAmt = function() {
+   
+ }
+ 
+ Bank.prototype.deposit = function(deposit) {
+   this.totalMoney = this.totalMoney + deposit;
+ }
+ 
+ Bank.prototype.withdrawals = function(withdrawal) {
+   this.totalMoney = this.totalMoney + withdrawal;
+ }
+ 
+ 
+ $(document).ready(function(){
+   $('form#new-account').submit(function(event) {
+     event.preventDefault();
+     let clientName = $('input#userName').val();
+     let initialBalance = $('input#initialDeposit').val();
+ 
+     let userBank = new Bank(clientName, initialBalance);
+ 
+     Bank.withdrawals($('input#giveMeMoney').val())
+     Bank.deposit($('input#takeMyMoney').val())
+     
+     $('.balanceDisplay').text('$' + userBank.totalMoney);
+     $('.userName').text(clientName);
+   })
+ })
+ 
+ 

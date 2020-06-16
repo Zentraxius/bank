e5b805ca67b49c6206ae813f305639471c27ffc9
Cody Fritz <clanalia55@gmail.com>
2020-06-16 14:15:20 -0700

    add initial html elements and basics of js
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>
    co-authored by: Tristan Emmerson <tristan@stickerslug.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-// constructor and proto
-// initial deposit, withdrawl, depo/balanace
-// BankAccount constructor
-// darth plagueis story... 
-// A long time ago in a galaxy far, far away...
-
-
-//   buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
-
-// please fucking reee, this is bs, on phone
-// I'm so sorry, i hate this
-//im angry *and* dissapointed
-// waiting a call back in the next ~5 mins or a knock at the door
-// am scared. cat is screaming at me
-
-//its okay man. 
-// no it's noT. NO IT'S NOT! GOD WILL PUNISH YOU
-//call the judge a name. that usually goes well.
-// dont listen to her. she's got rage problems. :-/
-// IT'S NOT MY RETRIBUTION. IT'S GOD'S THAT YOU WILL FEEL
-//I'M NOT ANGRY. JUST DISAPPOINTED.
-//her capslock argues otherwise
-
 function Bank() {
  this.accountName = accountName;
  this.startingAmt = startingAmt;
@@ -43,7 +20,14 @@ Bank.prototype.withdrawl = function() {
 
 
 $(document).ready(function(){
-  $('inserthing').submit(function(event) {
+  $('form#new-account').submit(function(event) {
     event.preventDefault();
+
+    let clientName = $('input#userName').val()
+    let initalBalance = ($('input#initialDeposit').val() + $('#giveMeMoney').val() - $('#takeMyMoney').val())
+
+
+    $('.balanceDisplay').text('$'+initalBalance)
+    $('.userName').text(clientName)
   })
 })

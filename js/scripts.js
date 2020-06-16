228bf5e71088442e25f7acd6d6ce9f242d0bb58b
Cody Fritz <clanalia55@gmail.com>
2020-06-16 13:37:34 -0700

    add initial files and setup+readme
    Co-authored by: Kelly Eidsvik <kelly.eidsvik@gmail.com>
    co-authored by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+// constructor and proto
+// initial deposit, withdrawl, depo/balanace
+// BankAccount constructor
+// darth plagueis story... 
+// A long time ago in a galaxy far, far away...
+
+
+//   buttons.append("<button class='deleteButton' id=" + contact.id + ">Delete</button>");
+
+// please fucking reee, this is bs, on phone
+// I'm so sorry, i hate this
+//im angry *and* dissapointed
+// waiting a call back in the next ~5 mins or a knock at the door
+// am scared. cat is screaming at me
+
+//its okay man. 
+// no it's noT. NO IT'S NOT! GOD WILL PUNISH YOU
+//call the judge a name. that usually goes well.
+// dont listen to her. she's got rage problems. :-/
+// IT'S NOT MY RETRIBUTION. IT'S GOD'S THAT YOU WILL FEEL
+//I'M NOT ANGRY. JUST DISAPPOINTED.
+//her capslock argues otherwise
+
+
+
+$(document).ready(function(){
+  $('inserthing').submit(function(event) {
+    event.preventDefault();
+  })
+})
